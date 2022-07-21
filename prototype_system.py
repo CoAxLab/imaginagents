18a77104bafddc09a719e2220759b2719619707b
Jack <jack.l.burgess.20@dartmouth.edu>
2022-07-21 10:29:11 -0400

    Added visialization for performance across gamma settings

--- prototype_system.py
+++ prototype_system.py
@@ -8,6 +8,7 @@ Created on Tue Jul 19 2022
 
 import numpy as np
 from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
 
 
 # Environment class
@@ -46,18 +47,30 @@ class Learner:
                       self.env.rng.choice(2, self.env.sample_size))
     
     # run learner on environment
-    def run(self, n_runs):
-        for run in range(n_runs):
+    def run(self, params):
+        
+        m_e_scores = []
+        m_i_scores = []
+        m_c_scores = []
+        
+        for run in range(params['n_runs']):
             (X_w, Y_w) = self.env.sample()
             #print(self.evaluate(X_w, Y_w)) # print current score
-            self.update(X_w, Y_w)
+            
+            m_e_scores.append(self.m_e.score(X_w.reshape(-1, 1), Y_w))
+            m_i_scores.append(self.m_i.score(X_w.reshape(-1, 1), Y_w))
+            m_c_scores.append(self.m_c.score(X_w.reshape(-1, 1), Y_w))
+            
+            self.update(X_w, Y_w, params)
+            
+        return (m_e_scores, m_i_scores, m_c_scores)
     
     # evaluate current state
     def evaluate(self, X_w, Y_w):
         return self.m_c.score(X_w.reshape(-1, 1), Y_w)
     
     # update current state
-    def update(self, X_w, Y_w):
+    def update(self, X_w, Y_w, params):
         # calculate environment and imagination model losses
         Y_hat_e = self.m_e.predict(X_w.reshape(-1, 1))
         loss_e = np.sum( (Y_w - Y_hat_e) ** 2)
@@ -66,10 +79,10 @@ class Learner:
         phi = loss_e / (loss_e + loss_i) # relative loss proportion
         
         #print("phi = " + str(phi))
-        print(self.m_e.score(X_w.reshape(-1, 1), Y_w))
+        #print(self.m_e.score(X_w.reshape(-1, 1), Y_w))
         #print(self.m_i.score(X_w.reshape(-1, 1), Y_w))
-        print(self.m_c.score(X_w.reshape(-1, 1), Y_w))
-        print()
+        #print(self.m_c.score(X_w.reshape(-1, 1), Y_w))
+        #print()
         
         # train environment model on environment data
         self.m_e.fit(X_w.reshape(-1, 1), Y_w)
@@ -83,12 +96,13 @@ class Learner:
         theta_i = (self.m_i.coef_, self.m_i.intercept_)
         theta_c = (self.m_c.coef_, self.m_c.intercept_)
         
-        phi = 0
+        if 'phi' in params:
+            phi = params['phi']
         
         theta_x = (theta_e[0] * (1 - phi) + theta_i[0] * phi,
                    theta_e[1] * (1 - phi) + theta_i[1] * phi)
         
-        gam = 0.1
+        gam = params['gam']
         
         theta_c = (theta_x[0] * (1 - gam) + theta_c[0] * gam,
                    theta_x[1] * (1 - gam) + theta_c[1] * gam)
@@ -96,13 +110,23 @@ class Learner:
         (self.m_c.coef_, self.m_c.intercept_) = theta_c
         
         #print(theta_e)
-        #print(theta_e[0] * 0.5)
-        
         #print(sample_w)
-        pass
 
 
 
-e = Env(sample_size=10)
-l = Learner(e)
-l.run(n_runs=20)
+plt.figure()
+plt.xlabel('batch')
+plt.ylabel('R ^ 2')
+plt.ylim(-1,1)
+
+for gam in np.arange(0, 1, 0.25):
+    params = {'n_runs': 30, 'phi': 0, 'gam': gam}
+    
+    e = Env(sample_size=10)
+    l = Learner(e)
+    results = l.run(params)
+    
+    
+    #plt.plot(results[0], label='M_e')
+    plt.plot(results[2], label=('gam = ' + str(gam)))
+    plt.legend()
